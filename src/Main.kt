d9f5de38fa44391c3fb80fd24160fb2fd206a317
RamandeepNahar <ramandeep.s.nahar@gmail.com>
2024-09-08 13:35:48 -0400

    Added all the classes and the interface Dimensionable

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,41 @@
-abstract class Shape (_name : String) : Dimensionable{
+
+interface Dimensionable {
+    fun getArea(): Double
+}
+
+abstract class Shape(private val _name: String) : Dimensionable {
     var name = _name
-    fun getArea() {
-        return 0.0;
+    abstract override fun getArea(): Double
+}
+
+class Square(name: String, private val side: Double) : Shape(name) {
+    override fun getArea(): Double {
+        return 0.0 //change this
+    }
+}
+
+class Circle(name: String, private val radius: Double) : Shape(name) {
+    override fun getArea(): Double {
+        return 0.0 //change this
+    }
+}
+
+open class Triangle(name: String, private val base: Double, private val height: Double) : Shape(name) {
+    override fun getArea(): Double {
+        return 0.0 //change this
+    }
+}
+
+class EquilateralTriangle(name: String, side: Double) : Triangle(name, side, calculateHeight(side)) {
+
+    companion object {
+        // Calculates the height of an equilateral triangle given the side length
+        fun calculateHeight(side: Double): Double {
+            return 0.0 //change this
+        }
     }
 }
 
 fun main() {
-    println("Hello World!")
+    //add
 }
